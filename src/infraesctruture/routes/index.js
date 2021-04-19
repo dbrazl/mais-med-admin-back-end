addeed1f34f9e95ea8594487adef34d4936458c5
dbrazl <danielbrazlopes16@gmail.com>
2021-04-19 14:35:07 -0300

    feat: create post users route

--- src/infraesctruture/routes/index.js
+++ src/infraesctruture/routes/index.js
@@ -4,12 +4,15 @@ import PharmsController from "../../application/controllers/PharmsController";
 import MedicinesController from "../../application/controllers/MedicinesController";
 import VacinationsController from "../../application/controllers/VacinationsController";
 import VacineSchedulingController from "../../application/controllers/VacineSchedulingController";
+import UserController from "../../application/controllers/UserController";
 
 import MedicinesIndexValidator from "../../application/middlewares/validators/MedicinesController/IndexValidator";
+import UserStoreValidator from "../../application/middlewares/validators/UserController/StoreValidator";
 
 const app = new Router();
 
 app.get("/healthcheck", (request, response) => response.status(200).json());
 app.get("/medicines", MedicinesIndexValidator, MedicinesController.index);
+app.post("/users", UserStoreValidator, UserController.store);
 
 export default app;
