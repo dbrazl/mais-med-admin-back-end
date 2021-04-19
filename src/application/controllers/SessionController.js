c6ad2c374af0754b97a48ba5919d7fa6507633d9
dbrazl <danielbrazlopes16@gmail.com>
2021-04-19 16:05:33 -0300

    feat: create session controller

---
+++ src/application/controllers/SessionController.js
@@ -0,0 +1,41 @@
+import { Pharms, checkPassword } from "../models/Pharms";
+import jwt from "jsonwebtoken";
+import authConfig from "../../infraesctruture/config/authorization";
+
+class SessionController {
+  async store(request, response) {
+    try {
+      const { email, password } = request.body;
+
+      const pharm = await Pharms.findOne({ email });
+
+      if (!pharm)
+        return response.status(404).json({
+          message: "Not found",
+          reasons: ["User does not exist"],
+        });
+
+      const samePassword = await checkPassword({
+        password,
+        hash: pharm.password,
+      });
+
+      if (!samePassword)
+        return response.status(401).json({
+          message: "No authorizated",
+          reasons: ["Worng password"],
+        });
+
+      const token = await jwt.sign({ id: _pharm.id }, authConfig.secret);
+
+      return response.status(200).json({
+        email,
+        token,
+      });
+    } catch (error) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
+}
+
+export default new SessionController();
