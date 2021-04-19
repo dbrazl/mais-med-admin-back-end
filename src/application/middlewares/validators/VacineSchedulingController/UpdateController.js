4b92c0bdc875418c98b14e6e40e63cc73f93850e
dbrazl <danielbrazlopes16@gmail.com>
2021-04-19 18:01:36 -0300

    feat: create update validato of vacine scheduling

---
+++ src/application/middlewares/validators/VacineSchedulingController/UpdateController.js
@@ -0,0 +1,54 @@
+import * as zod from "zod";
+import { CustomError } from "../../../../services/customError";
+import { errorHandler } from "../../helpers/handlers";
+
+async function UpdateValidator(request, response, next) {
+  try {
+    const schema = zod.object({
+      date: zod.string(),
+      schedule: zod.string(),
+      medicineId: zod.string(),
+    });
+    schema.parse(request.body);
+
+    const { date, schedule, medicineId } = request.body;
+
+    const dateSchema = zod
+      .string()
+      .refine((data) => data.match(/\//g)?.length === 2 && data.split("/")[2], {
+        path: ["date"],
+        message: "Date is malformmed",
+      });
+    dateSchema.parse(date);
+
+    const SCHEDULE_LENGTH = 19;
+
+    if (schedule.length !== SCHEDULE_LENGTH)
+      throw new CustomError({
+        errors: [
+          {
+            path: ["schedule"],
+            message: `Schedule is malformed - Index: ${index}`,
+          },
+        ],
+      });
+
+    const MEDICINE_ID_LEGNTH = 24;
+
+    if (medicineId.length !== MEDICINE_ID_LEGNTH)
+      throw new CustomError({
+        errors: [
+          {
+            path: ["medicineId"],
+            message: "Medicine id is malformed",
+          },
+        ],
+      });
+
+    return next();
+  } catch (error) {
+    return response.status(400).json(errorHandler(error));
+  }
+}
+
+export default UpdateValidator;
