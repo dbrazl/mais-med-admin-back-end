e1e47ac07f263715de7aa4a749bbc5cdddf1cc72
dbrazl <danielbrazlopes16@gmail.com>
2021-04-19 18:05:38 -0300

    feat: create update method of vacine scheduling

--- src/application/controllers/VacineSchedulingController.js
+++ src/application/controllers/VacineSchedulingController.js
@@ -34,6 +34,29 @@ class VacineSchedulingController {
       return response.status(500).json({ mesage: error.message });
     }
   }
+
+  async update(request, response) {
+    try {
+      const { date, schedule, medicineId } = request.body;
+
+      const scheduling = await VacineScheduling.findOne({ medicineId, date });
+
+      if (!scheduling)
+        return response.status(401).json({
+          message: "Not found",
+          reasons: ["Scheduling is not registered"],
+        });
+
+      scheduling.date = date;
+      scheduling.schedule = schedule;
+      scheduling.medicineId = medicineId;
+      const schedulingUpdated = await scheduling.save();
+
+      return response.status(200).json(schedulingUpdated);
+    } catch (error) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
 }
 
 export default new VacineSchedulingController();
