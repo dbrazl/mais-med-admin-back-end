d726a77f292b3745bd07e65c8e244f7d259b12aa
dbrazl <danielbrazlopes16@gmail.com>
2021-04-19 17:14:48 -0300

    feat: create route to store medicines

--- src/infraesctruture/routes/index.js
+++ src/infraesctruture/routes/index.js
@@ -11,6 +11,7 @@ import PharmsStoreValidator from "../../application/middlewares/validators/Pharm
 import PharmsUpdateValidator from "../../application/middlewares/validators/PharmsController/UpdateValidator";
 import SessionStoreValidator from "../../application/middlewares/validators/SessionController/StoreValidator";
 import VacinantionIndexValidator from "../../application/middlewares/validators/VacinationsController/IndexValdiator";
+import MedicinesStoreValidator from "../../application/middlewares/validators/MedicinesController/StoreValidator";
 
 import authorizationMiddleware from "../../application/middlewares/Authorization/JWT";
 
@@ -25,5 +26,6 @@ app.get("/medicines", MedicinesIndexValidator, MedicinesController.index);
 app.post("/pharms", PharmsStoreValidator, PharmsController.store);
 app.put("/pharms", PharmsUpdateValidator, PharmsController.update);
 app.get("/vacinations", VacinantionIndexValidator, VacinationsController.index);
+app.post("/medicines", MedicinesStoreValidator, MedicinesController.store);
 
 export default app;
