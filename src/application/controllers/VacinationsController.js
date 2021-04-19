6c3e317ff65826d286009502d5cc4b4602a26527
dbrazl <danielbrazlopes16@gmail.com>
2021-04-19 16:58:42 -0300

    feat: create index method of vacination controller

--- src/application/controllers/VacinationsController.js
+++ src/application/controllers/VacinationsController.js
@@ -1,6 +1,25 @@
 import { Vacination, encodeCpf } from "../models/Vacination";
 import { VacineScheduling } from "../models/VacineScheduling";
 
-class VacinationController {}
+class VacinationController {
+  async index(request, response) {
+    const { page } = request.query;
+    const { id } = request.user;
+    const offset = parseInt(page) * 10;
+
+    const vacinations = await Vacination.find({ unityId: id })
+      .limit(10)
+      .skip(offset)
+      .select("-_id -__v");
+
+    if (vacinations.length <= 0)
+      return response.status(404).json({
+        message: "Not found",
+        reasons: ["Don't have vacinations on this page"],
+      });
+
+    return response.status(200).json(vacination);
+  }
+}
 
 export default new VacinationController();
