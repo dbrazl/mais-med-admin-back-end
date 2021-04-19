633f4fdaf6a4f434ef8e9e9d42ea6807b363c176
dbrazl <danielbrazlopes16@gmail.com>
2021-04-19 14:25:18 -0300

    feat: create user model

---
+++ src/application/models/User.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import { encodeConfig } from "../../infraesctruture/config/encode";
+
+const schema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+function encodePassword(password = "") {
+  return bcrypt.hash(password, encodeConfig.encodeLength);
+}
+
+const User = model("User", schema);
+
+export default { User, encodePassword };
