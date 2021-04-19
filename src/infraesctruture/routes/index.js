695b67013b49b2185ef38d0d76b5e3ac6fdf72fc
dbrazl <danielbrazlopes16@gmail.com>
2021-04-19 16:10:13 -0300

    feat: create store vaidator sesssion and sessio route

--- src/infraesctruture/routes/index.js
+++ src/infraesctruture/routes/index.js
@@ -2,18 +2,21 @@ import { Router } from "express";
 
 import PharmsController from "../../application/controllers/PharmsController";
 import MedicinesController from "../../application/controllers/MedicinesController";
+import SessionController from "../../application/controllers/SessionController";
 import VacinationsController from "../../application/controllers/VacinationsController";
 import VacineSchedulingController from "../../application/controllers/VacineSchedulingController";
 
 import MedicinesIndexValidator from "../../application/middlewares/validators/MedicinesController/IndexValidator";
 import PharmsStoreValidator from "../../application/middlewares/validators/PharmsController/StoreValidator";
 import PharmsUpdateValidator from "../../application/middlewares/validators/PharmsController/UpdateValidator";
+import SessionStoreValidator from "../../application/middlewares/validators/SessionController/StoreValidator";
 
 import authorizationMiddleware from "../../application/middlewares/Authorization/JWT";
 
 const app = new Router();
 
 app.get("/healthcheck", (request, response) => response.status(200).json());
+app.post("/session", SessionStoreValidator, SessionController.store);
 
 app.use(authorizationMiddleware);
 
